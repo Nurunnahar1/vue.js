cc2cba97b25977955e42c1a9f39b3d7eef95ca23
Nurunnahar <nurunnahar9081@gmail.com>
2023-10-26 11:32:00 +0600

    style binding

--- vue4/src/App.vue
+++ vue4/src/App.vue
@@ -1,21 +1,39 @@
 <script setup>
-const tasks = [
-  "Complete project proposal",
-  "Update website content",
-  "Fix bug in user authentication",
-  "Prepare presentation slides",
-  "Test new feature implementation",
-];
+  const bgOrange="bg-orange-400"
+  const bgIndigo="bg-indigo-400"
+  const bgGreen="bg-green-400"
+
+ const weekday = false
 </script>
 
 <template>
-  <section class="mx-auto container">
-    <section class="mx-auto container space-y-3">
-        <div class="p-5 border border-gray-600 text-left" v-for="(task,index) in tasks" >{{ index+1 }}. {{ task }}</div>
+  <section class="container mx-auto">
+    <h1 class="text-center text-3xl py-12">Class & Style Binding</h1>
+
+    <section class="flex space-x-10">
+      <div class="w-1/3 h-16" :class="bgOrange"></div>
+      <div class="w-1/3 h-16" :class="bgIndigo" ></div>
+      <div class="w-1/3 h-16" :class="bgGreen"></div>
+    </section>
+
+    <section class="mt-10">
+      <!-- <div class="h-16 flex items-center justify-center text-white" :class="weekday?'bg-red-600 text-2xl':'bg-green-600 text-4xl'">
+        It's a weekday!
+      </div> -->
+
+
+      <div class="h-16 flex items-center justify-center text-white" :class="[weekday?'bg-red-600 ':'bg-green-600 ', weekday==true?'text-2xl':'text-4xl']">
+        It's a weekday!
+      </div>
+    </section>
+
+    <section class="mt-10">
+      <div class=" border-t-4 rounded-b px-4 py-3 shadow-md"  >
+        <p class="font-bold">Our privacy policy has changed</p>
+        <p class="text-sm">Make sure you know how these changes affect you.</p>
+      </div>
     </section>
   </section>
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
