2d439d403b7f02dcf4232ed07eefc6a74e8c0ae7
Nurunnahar <nurunnahar9081@gmail.com>
2023-10-26 11:45:48 +0600

    condition wise style bindin

--- vue4/src/App.vue
+++ vue4/src/App.vue
@@ -4,6 +4,29 @@
   const bgGreen="bg-green-400"
 
  const weekday = false
+
+
+const colors = {
+  bgColor:'bg-teal-100',
+  borderColor:'border-teal-500',
+  textColor:'text-teal-900',
+}
+
+const messageType = 'warning'
+
+if('error'==messageType){
+  colors.bgColor='bg-red-100',
+  colors.borderColor='border-red-500',
+  colors.textColor='text-red-900'
+}
+if('warning'==messageType){
+  colors.bgColor='bg-orange-100',
+  colors.borderColor='border-orange-500',
+  colors.textColor='text-orange-900'
+}
+
+
+
 </script>
 
 <template>
@@ -28,7 +51,7 @@
     </section>
 
     <section class="mt-10">
-      <div class=" border-t-4 rounded-b px-4 py-3 shadow-md"  >
+      <div class=" border-t-4 rounded-b px-4 py-3 shadow-md"  :class="[colors.bgColor,colors.borderColor,colors.textColor]">
         <p class="font-bold">Our privacy policy has changed</p>
         <p class="text-sm">Make sure you know how these changes affect you.</p>
       </div>
