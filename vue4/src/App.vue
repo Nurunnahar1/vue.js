2532fbe5baf7a40018cb0298021c9c314dfeace3
Nurunnahar <nurunnahar9081@gmail.com>
2023-10-28 18:38:51 +0600

    script setup (reactivity)

--- vue4/src/App.vue
+++ vue4/src/App.vue
@@ -1,52 +1,18 @@
 <script setup>
-const bgOrange = 'background-color: rgba(251, 146, 60, 1)'
-const bgIndigo = 'background-color: rgba(129, 140, 248, 1)'
-const bgGreen = 'background-color: rgba(52, 211, 153, 1)'
+import { ref } from "vue";
 
-const bgRed = 'background-color: rgba(220, 38, 38, 1)'
-const bgDarkGreen = 'background-color: rgba(5, 150, 105, 1)'
-
-const colors = {
-  borderColor: 'rgba(20, 184, 166, 1)', //rgba(239, 68, 68,1)
-  // 'border-color': 'rgba(20, 184, 166, 1)', //rgba(239, 68, 68,1)
-  color: 'rgba(19, 78, 74, 1)', //rgba(127, 29, 29,1)
-  backgroundColor: 'rgba(204, 251, 241, 1)' //rgba(254, 226, 226,1)
-}
-
-const weekday = true
-const messageType = 'error'
-
-if('error'==messageType){
-  colors.borderColor='rgba(239, 68, 68,1)'
-  colors.backgroundColor = 'rgba(254, 226, 226,1)'
-  colors.color = 'rgba(127, 29, 29,1)'
-}
+ let courseName = ref("Vue js masterClass");
+let count= ref(0);
 </script>
 
 <template>
-  <section class="container mx-auto">
-    <h1 class="text-center text-3xl py-12">Class & Style Binding</h1>
+ <h1>Course Name: {{ courseName }}</h1>
 
-    <section class="flex space-x-10">
-      <div class="w-1/3 h-16" :style="bgOrange">hello 1</div>
-      <div class="w-1/3 h-16" :style="bgIndigo">hello 2</div>
-      <div class="w-1/3 h-16" :style="bgGreen">hello 3</div>
-    </section>
-
-    <section class="mt-10">
-      <div class="h-16 flex items-center justify-center text-white"  >Hello</div>
-      <div class="h-16 flex items-center justify-center text-white"  :style="weekday==true?[bgRed,'font-size:20px']:[bgDarkGreen, 'font-size:30px']">
-        It's a weekday!
-      </div>
-    </section>
-
-    <section class="mt-10">
-      <div class=" border-t-4  rounded-b  px-4 py-3 shadow-md" :style="colors">
-        <p class="font-bold">Our privacy policy has changed</p>
-        <p class="text-sm">Make sure you know how these changes affect you.</p>
-      </div>
-    </section>
-  </section>
+ <button @click="count++">+</button>
+ <h3>{{ count }}</h3>
+ <button @click="count--">-</button>
 </template>
 
-<style scoped></style>
+<style scoped>
+
+</style>
