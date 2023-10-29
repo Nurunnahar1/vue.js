a720ab37c66be44d745ba449d50e37287ba25453
Nurunnahar <nurunnahar9081@gmail.com>
2023-10-29 17:01:04 +0600

    display index in loop

--- vue4/src/App.vue
+++ vue4/src/App.vue
@@ -1,18 +1,25 @@
 <script setup>
-import { ref } from "vue";
-
- let courseName = ref("Vue js masterClass");
-let count= ref(0);
+const tasks = [
+  "Complete project proposal",
+  "Update website content",
+  "Fix bug in user authentication",
+  "Prepare presentation slides",
+  "Test new feature implementation",
+];
 </script>
 
 <template>
- <h1>Course Name: {{ courseName }}</h1>
-
- <button @click="count++">+</button>
- <h3>{{ count }}</h3>
- <button @click="count--">-</button>
+  <section class="mx-auto container">
+    <section class="mx-auto container space-y-3">
+      <div
+        class="p-5 border border-gray-600 text-left"
+        v-for=" (task, index) in tasks "
+      >
+       {{ index+1 }}.{{ task }}
+      </div>
+    </section>
+  </section>
 </template>
 
 <style scoped>
-
 </style>
