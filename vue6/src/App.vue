220504de89fdf7549e443c43c078388d595949b8
Nurunnahar <nurunnahar9081@gmail.com>
2023-11-03 23:10:18 +0600

    show data and change taht by using reactive (object data)

--- vue6/src/App.vue
+++ vue6/src/App.vue
@@ -1,27 +1,23 @@
 <script setup>
- import { ref } from 'vue';
-
-const color = ref('None')
-
-function getStyle() {
-  return {
-    backgroundColor: color.value,
-  }
-}
+ import {ref, reactive} from 'vue'
+ const persons = reactive({
+  name:'Nasrin',
+  age:28,
+  job:'Web Developer'
+ })
 </script>
 
 <template>
   <section class="mx-auto container">
     <h1 class="text-2xl mb-10">Vue Form</h1>
-    <p class="pb-5">Your Favorite Color: {{ color }} </p>
-    <p>
-      Your Favorite Color:
-      <input class="p-5"  v-model="color" />
-      <div class="w-32 h-32 mt-10" :style="getStyle()"  ></div>
-    </p>
+    <p class="mb-10">{{ persons }}</p>
+ 
+
+    <div class="flex flex-col mb-5 text-left"  v-for="(value,key,index) in persons" :key="key">
+      <label for="name" >{{ key }}</label> 
+      <input type="text" class="border border-gray-300 rounded-md p-2" v-model="persons[key]" />
+    </div>
+
   </section>
 </template>
-
-<style scoped>
-
-</style>
+<style scoped></style>
