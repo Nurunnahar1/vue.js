459e30c9db136c8ae1d4e86e652f10e106e7c779
Nurunnahar <nurunnahar9081@gmail.com>
2023-11-03 22:14:17 +0600

    give a data in an input and display that

--- vue5/src/App.vue
+++ vue5/src/App.vue
@@ -1,61 +1,55 @@
 <script setup>
-import { ref, reactive } from "vue";
+ import { ref, reactive } from 'vue';
 
-const countries = reactive([
-  { name: "Bangladesh", capital: "Dhake" },
-  { name: "India", capital: "Dilli" },
-]);
+ const countries = reactive([
+  {name:"Bangladesh", capital:"Dhaka"}
+ ])
 
-function getAllCountries(List = 'All') {
+ function getAllCountries (list = 'All') {
   countries.length=0
-  fetch("https://restcountries.com/v3.1/all?fields=name,capital")
-    .then((Response) => Response.json())
-    .then((data) => {
-      if('All'==List){
-        data.forEach((country) => {
-        countries.push({
-          name: country.name.common,
-          capital: country.capital[0],
-        });
-      });
-      }
-      else{
-        data.filter(c=>c.name.common.startsWith(List)).forEach(country=>{
+  fetch('https://restcountries.com/v3.1/all?fields=name,capital')
+    .then(response => response.json())
+    .then(data => {
+      if ('All' == list) {
+        data.forEach(country => {
           countries.push({
             name: country.name.common,
-            capital:country.capital[0]
+            capital: country.capital[0]
           })
-      })
-
-
-    }
-  })
-
+        })
+      }else{
+        data.filter(c=>c.name.common.startsWith(list)).forEach(country => {
+          countries.push({
+            name: country.name.common,
+            capital: country.capital[0]
+          })
+        })
+      }
+    })
 }
-
 </script>
 
 <template>
   <section class="container mx-auto">
     <h1 class="text-2xl">Country List</h1>
     <button
-      class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded w-48 mt-5"
-      @click="getAllCountries()"
+      class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded w-48 mt-5" @click="getAllCountries()"
+     
     >
-      List of Countries
+      List of Countries :
     </button>
     <button
-      class="w-10 ml-2 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-5" @click="getAllCountries('A')"
+      class="w-10 ml-2 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-5" 
     >
       A
     </button>
     <button
-      class="w-10 ml-2 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-5" @click="getAllCountries('B')"
+      class="w-10 ml-2 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-5"  
     >
       B
     </button>
     <button
-      class="w-10 ml-2 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-5"  @click="getAllCountries('C')"
+      class="w-10 ml-2 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-5"  
     >
       C
     </button>
@@ -63,7 +57,8 @@ function getAllCountries(List = 'All') {
 
     <ul class="mt-10">
       <li v-for="country in countries" :key="country.name">
-        {{ country.name }} = {{ country.capital }}
+      {{ country.name }} = {{ country.capital }}
+        
       </li>
     </ul>
   </section>
