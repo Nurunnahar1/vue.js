5d8fc12f7fdddac121bdd90611c6a08ce848b653
Nurunnahar <nurunnahar9081@gmail.com>
2023-11-08 00:07:55 +0600

    quiz_application done

--- quiz_application/src/App.vue
+++ quiz_application/src/App.vue
@@ -20,89 +20,102 @@ const questions = ref([
     options: ["Dove", "Magpie", "Parrot", "Pigeon"],
     selected: null,
   },
-
 ]);
 
-const questionCompleted = ref(false)
-const currentQuestion = ref(0)
-const score = computed(()=>{
-    let value = 0
-    questions.value.map(question =>{
-      if(question.selected == question.answer){
-        value++
-      }
-      return value
-    })
-})
+const quizCompleted = ref(false);
+const currentQuestion = ref(0);
 
-const getCurrentQuestion = computed(()=>{
-  let question = questions.value[currentQuestion.value]
-  question.index = currentQuestion.value
-  return question
-})
 
-const setAnswer = evt =>{
-  questions.value[currentQuestion.value].selected = evt. target.value
-  evt.target.value = null
-}
+const score = computed(() => {
+  let value = 0;
+  questions.value.map((question) => {
+    if (question.selected == question.answer) {
+      value++;
+    }
+    
+  });
+  return value;
+});
 
-const nextQuestion = ()=>{
-  if(currentQuestion.value < questions.value.length -1){
-    currentQuestion.value++
+
+const getCurrentQuestion = computed(() => {
+  let question = questions.value[currentQuestion.value];
+  question.index = currentQuestion.value;
+  return question;
+});
+
+const setAnswer = evt => {
+  questions.value[currentQuestion.value].selected = evt.target.value;
+  evt.target.value = null;
+};
+
+const nextQuestion = () => {
+  if (currentQuestion.value < questions.value.length - 1) {
+    currentQuestion.value++;
+  } else {
+    quizCompleted.value = true;
   }
-  else{
-    questionCompleted.value = true 
-  }
-}
+};
 </script>
 
 <template>
   <main class="app">
-  <h1>The Quiz</h1>
+    <h1>The Quiz</h1>
 
-  <section class="quiz">
-    <div class="quiz-info">
-      <span class="question">{{ getCurrentQuestion.question }}</span>
-      <span class="score">Score {{ score }}/{{ questions.length }}</span>
-    </div>
+    <section class="quiz" v-if="!quizCompleted">
+      <div class="quiz-info">
+        <span class="question">{{ getCurrentQuestion.question }}</span>
+        <span class="score">Score {{ score }}/{{ questions.length }}</span>
+      </div>
 
-    <div class="options">
-      <label v-for="(option, index) in getCurrentQuestion.options"
-        :key="index"
-        :class="`option ${
+      <div class="options">
+        <label
+          v-for="(option, index) in getCurrentQuestion.options"
+          :key="index"
+          :class="`option ${
             getCurrentQuestion.selected == index
-              ?index == getCurrentQuestion.answer
-                ? 'correct':'wrong':''
-        } ${
-          getCurrentQuestion.selected !=null && index !=getCurrentQuestion.selected ? 'disabled' : ''
-        }
+              ? index == getCurrentQuestion.answer
+                ? 'correct'
+                : 'wrong'
+              : ''
+          } ${
+            getCurrentQuestion.selected != null &&
+            index != getCurrentQuestion.selected
+              ? 'disabled'
+              : ''
+          }
         
         
-        `" >
-          
-            <input type="radio" 
-            :name="getCurrentQuestion.index" 
+        `"
+        >
+          <input
+            type="radio"
+            :name="getCurrentQuestion.index"
             :value="index"
             v-model="getCurrentQuestion.selected"
             :disabled="getCurrentQuestion.selected"
-            @change="setAnswer" >
+            @change="setAnswer"
+          />
 
-            <span>{{ option }}</span>
-      </label>
-    </div>
-    <button
-      @click="nextQuestion"
-      :disabled="!getCurrentQuestion.selected" >
-      {{ 
-        getCurrentQuestion.index == questions.length - 1
-         ? 'Finish'
-         :getCurrentQuestion.selected == null
-          ? 'Select an option'
-          : 'Next Question'
-       }}
-    </button>
-  </section>
-  
+          <span>{{ option }}</span>
+        </label>
+      </div>
+      <button @click="nextQuestion" :disabled="!getCurrentQuestion.selected">
+        {{
+          getCurrentQuestion.index == questions.length - 1
+            ? "Finish"
+            : getCurrentQuestion.selected == null
+            ? "Select an option"
+            : "Next Question"
+        }}
+      </button>
+    </section>
+
+    <section v-else>
+      <h2>You have finished the quiz!!</h2>
+      <p>Your score is {{ score }}/ {{ questions.length }}</p>
+    
+    </section>
   </main>
 </template>
 
@@ -117,5 +130,108 @@ const nextQuestion = ()=>{
 body {
   background-color: #271c36;
   color: #fff;
+}
+
+.app{
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 2rem;
+  min-height: 100vh;
+}
+
+h1{
+  font-size: 2rem;
+  margin-bottom: 2rem;
+}
+
+.quiz{
+  background-color: #382a4b;
+  padding: 1rem;
+  width: 100%;
+  max-width: 640px;
+  border-radius: .5rem;
+}
+
+.quiz-info{
+  display: flex;
+  justify-content: space-between;
+  margin-bottom: 1rem;
+}
+
+.quiz-info .question{
+  color:#8f8f8f;
+  font-size: 1.25rem;
+}
+
+.quiz-info .score{
+  color: #fff;
+  font-size: 1.25rem;
+}
+
+.options{
+  margin-bottom: 1rem;
+}
+
+.option{
+  display: block;
+  padding: 1rem;
+  background-color: #271c36;
+  margin-bottom: 0.5rem;
+  border-radius: 0.5rem;
+  cursor: pointer;
+}
+
+.option:hover{
+  background-color: #2d213f;
+}
+
+.option.correct{
+  background-color: #2cce7d;
+}
+.option.wrong{
+  background-color: #ff5a5f;
+}
+
+.option:last-of-type{
+  margin-bottom: 0;
+}
+
+.option.disabled{
+  opacity: 0.5;
+}
+
+.option input{
+  display: none;
+}
+
+button{
+  appearance: none;
+  outline: none;
+  border: none;
+  cursor: pointer;
+
+  padding: 0.5rem 1rem;
+  background-color: #2cce7d;
+  color: #2d213f;
+  font-weight: 700;
+  text-transform: uppercase;
+  font-size: 1.25rem;
+  border-radius: 0.5rem;
+}
+
+button:disabled{
+  opacity: 0.5;
+}
+
+h2{
+  font-size: 2rem;
+  margin-bottom: 2rem;
+  text-align: center;
+}
+
+p{
+  color: #8f8f8f;
+  font-size: 1.25rem;
 }
 </style>
