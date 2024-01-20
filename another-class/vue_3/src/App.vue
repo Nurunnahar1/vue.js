8fb98441795e725b20f28f767ef3259db3681d1c
Nurunnahar <nurunnahar9081@gmail.com>
2024-01-20 11:27:41 +0600

    another class (ref reactive)

---
+++ another-class/vue_3/src/App.vue
@@ -0,0 +1,20 @@
+
+
+<script setup>
+import { ref, reactive } from "vue";
+let count = ref(0);
+
+count.value = 10;
+
+let course = reactive({
+  name: "Vue 3 Mastering",
+  mentor: "Nasrin",
+  plateform: "abc",
+});
+</script>
+
+<template>
+  <h1>{{ course.name }}</h1>
+  <h1>{{ course.mentor }}</h1>
+  <h1>{{ course.plateform }}</h1>
+</template>
