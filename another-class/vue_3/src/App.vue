5f309827f1193d8121bff6758a03ba188b7e9c75
Nurunnahar <nurunnahar9081@gmail.com>
2024-01-21 15:11:45 +0600

    increment & decrement using function

--- another-class/vue_3/src/App.vue
+++ another-class/vue_3/src/App.vue
@@ -1,27 +1,27 @@
-
-
 <script setup>
 import { ref, reactive } from "vue";
 let count = ref(0);
-
-count.value = 10;
+let title = ref("Methods/Function in Vue");
 
 let course = reactive({
   name: "Vue 3 Mastering",
   mentor: "Nasrin",
   plateform: "abc",
 });
+
+function increment() {
+  count.value++;
+}
+function decrement() {
+  count.value--;
+}
 </script>
 
 <template>
-  <h1>{{ course.name }}</h1>
-  <h1>{{ course.mentor }}</h1>
-  <h1>{{ course.plateform }}</h1>
-
+  <h1>{{ course.name }} - {{ title }}</h1>
   <p>course by {{ course.name }}</p>
 
-  <button @click="count++" >+</button>
+  <button @click="increment">+</button>
   <h3>{{ count }}</h3>
-  <button @click="count--" >-</button>
-
+  <button @click="decrement">-</button>
 </template>
