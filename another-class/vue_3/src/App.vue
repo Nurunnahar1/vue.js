37e4d86cc5f20095e6cd4ad28fc46295851f420b
Nurunnahar <nurunnahar9081@gmail.com>
2024-01-29 11:10:32 +0600

    watch method

--- another-class/vue_3/src/App.vue
+++ another-class/vue_3/src/App.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { ref, reactive ,computed } from "vue";
+import { ref, reactive, computed, watch } from "vue";
 let count = ref(0);
 let title = ref("Methods/Function in Vue");
 
@@ -31,18 +31,31 @@ const changeName = () => (course.name = "Muskan");
 /** check count number is odd or even */
 
 const checkOddOrEven = computed(() => {
-  if( count.value % 2  === 0 ) return 'Even';
-  
-    return 'Odd';
+  // if( count.value % 2  === 0 ) return 'Even';
+  //   return 'Odd';
+  return count.value % 2 === 0 ? 'Odd' : 'Odd';
+});
 
-})
+
+/** watch method */
+watch(() => count.value, (newValue, oldValue)=> {
+  console.log('newValue', newValue);
+  console.log('oldValue', oldValue);
+
+  if (newValue > 4) {
+    alert('Count value is over than 4');
+  }
+});
+
+
+
 </script>
 
 <template>
   <h1>{{ course.name }} - {{ title }}</h1>
   <p @click="changeName">course by {{ fullName }}</p>
 
-  <button @click="increment(20)">+</button>
-  <h3>{{ count }}  // {{ checkOddOrEven }}</h3>
-  <button @click="decrement(5)">-</button>
+  <button @click="increment(1)">+</button>
+  <h3>{{ count }} // {{ checkOddOrEven }}</h3>
+  <button @click="decrement(1)">-</button>
 </template>
