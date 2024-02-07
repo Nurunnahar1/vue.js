ae21030a2fccfd65ef144c1af7f2de04dc9c415a
Nurunnahar <nurunnahar9081@gmail.com>
2024-02-07 08:31:03 +0600

    import component

--- another-class/vue_3/src/App.vue
+++ another-class/vue_3/src/App.vue
@@ -1,22 +1,14 @@
 <template>
   <h1>{{ title }}</h1>
-  <div class="">
-    <button @click="showModal = true">Show Modal</button>
-
-    <Teleport to="body">
-      <div v-if="showModal" class="modal">
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe, atque?
-        </p>
-        <button @click="showModal = false">Close</button>
-      </div>
-    </Teleport>
-  </div>
+ <Counter/>
 </template>
 
 <script setup>
-import { ref, nextTick } from "vue";
-let title = ref("VueJs Master Class");
+import { ref } from "vue";
+import Counter from "./components/Counter.vue";
 
-const showModal = ref(false);
+let title = ref("VueJs Master Class");
+ 
 </script>
+
+ 
---
+++ another-class/vue_3/src/components/Counter.vue
@@ -0,0 +1,23 @@
+<template>
+     <div class="counter">
+    <button @click="count++">+</button>
+    <p>{{ count }}</p>
+    <button @click="count--">-</button>
+  </div>
+</template>
+
+<script setup>
+import { ref, nextTick } from "vue";
+const count = ref(0);
+</script>
+ 
+
+
+<style scoped>
+.counter {
+  display: flex;
+  width: 100px;
+  justify-content: space-between;
+  padding: 10px;
+}
+</style>
