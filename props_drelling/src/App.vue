73254c4ac189a817b36664a671cf96e02f175252
Nurunnahar <nurunnahar9081@gmail.com>
2024-02-16 11:32:40 +0600

    props

---
+++ props_drelling/src/App.vue
@@ -0,0 +1,32 @@
+<script setup>
+    import HelloWorld from './components/HelloWorld.vue'
+    import { ref, reactive } from 'vue'
+    import ButtonComponent from './components/ButtonComponent.vue';
+
+    const msg = ref("Hello World");
+
+    function clickHandlerOne() {
+      msg.value = "Click One";
+    }
+
+    function clickHandlerTwo() {
+      msg.value = "Click Two";
+    }
+
+ </script>
+
+<template>
+ 
+
+ <h1>Slot</h1>
+ <ButtonComponent  :clickHandler="clickHandlerOne" />
+ <ButtonComponent  :clickHandler="clickHandlerTwo" />
+
+ 
+</template>
+
+
+
+<style scoped>
+ 
+</style>
---
+++ props_drelling/src/components/ButtonComponent.vue
@@ -0,0 +1,11 @@
+<script setup>
+import { ref } from 'vue'
+defineProps(['clickHandler', ])
+</script>
+<template>
+
+<p>
+<button type="button" class="btn btn-primary" @click="clickHandler()" >Click Me</button>
+</p>
+
+</template>
